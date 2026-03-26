cd1ef1b97a96aadb06c962b247a4169868a52066
Nathaser <nathanael.seropian@ensta-paris.fr>
2026-03-26 11:53:06 +0100

    test du code pour tracer les theta

--- importance_sampling.py
+++ importance_sampling.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def g(x, r, T, sigma, K, S0):
-    return np.exp(-r*T) * np.max(0, S0*np.exp((r-0.5*sigma**2)*T + sigma*np.sqrt(T)*x) - K)
+    return np.exp(-r*T) * np.maximum(0, S0*np.exp((r-0.5*sigma**2)*T + sigma*np.sqrt(T)*x) - K)
 
 def u(N, theta, X, r, T, sigma, K, S0):
     u = 0
@@ -46,13 +46,13 @@ def price_option_MC(S0, K, T, r, sigma, N):
     # N: number of Monte Carlo simulations (sample size)
 
     # Box-Muller
-    U = npr.random(N/2) # careful: N must be pair 
-    V = npr.random(N/2)
+    U = npr.random(N//2) # careful: N must be pair 
+    V = npr.random(N//2)
     X = np.sqrt(-2 * np.log(U)) * np.cos(2 * np.pi * V) 
     Y = np.sqrt(-2 * np.log(U)) * np.sin(2 * np.pi * V) 
     Z = np.concatenate((X,Y)) # combine the two arrays to get N standard normal random variables
     
-    payoff = np.array([g(x, r, T, sigma, K, S0) for x in Z])
+    payoff = g(Z, r, T, sigma, K, S0)
     MC_price = np.mean(payoff)
     
     # 95% confidence interval
@@ -70,16 +70,23 @@ def price_option_MC(S0, K, T, r, sigma, N):
     return MC_price, True_price, CI_up, CI_down, error
 
 # our criteria for finding the optimal N is the difference between the MC price and the true price, which should be less than epsilon
-def find_optimal_N(S0, K, T, r, sigma, epsilon=1e-3):
-    N = 10e6
+def find_optimal_N(S0, K, T, r, sigma, epsilon=1e-1):
+    N = int(10e6)
     price = price_option_MC(S0, K, T, r, sigma, N)
     while (abs(price[0] - price[1]) > epsilon):
         N *= 10
         price = price_option_MC(S0, K, T, r, sigma, N)
     return N
 
-def plot_theta(theta_0, N, X, r, T, sigma, K, S0, epsilon=1e-6, max_iter=1000):
-    theta_list = theta_newton_algo(theta_0, N, X, r, T, sigma, K, S0, epsilon, max_iter)[1]
+def plot_theta(theta_0, N, r, T, sigma, K, S0, epsilon=1e-6, max_iter=1000):
+     # Box-Muller
+    U = npr.random(N//2) # careful: N must be pair 
+    V = npr.random(N//2)
+    X = np.sqrt(-2 * np.log(U)) * np.cos(2 * np.pi * V) 
+    Y = np.sqrt(-2 * np.log(U)) * np.sin(2 * np.pi * V) 
+    Z = np.concatenate((X,Y)) # combine the two arrays to get N standard normal random variables
+    
+    theta_list = theta_newton_algo(theta_0, N, Z, r, T, sigma, K, S0, epsilon, max_iter)[1]
     index = []
     for i in range(len(theta_list)):
         index.append(i)
@@ -90,5 +97,18 @@ def plot_theta(theta_0, N, X, r, T, sigma, K, S0, epsilon=1e-6, max_iter=1000):
     plt.title('Convergence of theta in Newton\'s algorithm')
     plt.show()
 
+S0 = 1
+sigma = 0.3
+r = 0.01
+K = 1.2
+T = 2 
 
-      
+N_opt = find_optimal_N(S0, K, T, r, sigma)
+print("Optimal N: ", N_opt)
+MC_price, True_price, CI_up, CI_down, error = price_option_MC(S0, K, T, r, sigma, N_opt)
+print("MC price: ", MC_price)
+print("True price: ", True_price)
+print("95% confidence interval: [", CI_down, ", ", CI_up, "]")
+print("Error: ", error)
+
+plot_theta(0, N_opt, r, T, sigma, K, S0)
